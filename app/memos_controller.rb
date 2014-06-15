1fe8c68a7353842502cb06d9c2d85157e9d5d639
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-06-15 21:35:21 +0900

    Show Qiita posts

--- app/memos_controller.rb
+++ app/memos_controller.rb
@@ -1,6 +1,10 @@
 class MemosController < UITableViewController
   def viewDidLoad
     @item = ['a', 'b', 'c']
+    BW::HTTP.get('https://qiita.com/api/v1/items') do |response|
+      @item = BW::JSON.parse(response.body.to_s)
+      self.tableView.reloadData
+    end
     self.title = 'メモ一覧'
   end
 
@@ -11,11 +15,11 @@ class MemosController < UITableViewController
   CELL_ID = 'cell'
   def tableView(tv, cellForRowAtIndexPath:index_path)
     cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:CELL_ID)
-    cell.textLabel.text = @item[index_path.row]
+    cell.textLabel.text = @item[index_path.row]['title']
     cell
   end
 
   def tableView(tv, didSelectRowAtIndexPath:index_path)
-    p @item[index_path.row]
+    p @item[index_path.row]['body']
   end
 end
