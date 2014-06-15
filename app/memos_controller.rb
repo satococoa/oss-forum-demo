f9b809908ed91d67b396ab3350a1e81eeb6d7907
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-06-15 21:15:40 +0900

    Show plain chars in table view

--- app/memos_controller.rb
+++ app/memos_controller.rb
@@ -1,5 +1,21 @@
 class MemosController < UITableViewController
   def viewDidLoad
+    @item = ['a', 'b', 'c']
     self.title = 'メモ一覧'
+  end
+
+  def tableView(tv, numberOfRowsInSection:section)
+    @item.count
+  end
+
+  CELL_ID = 'cell'
+  def tableView(tv, cellForRowAtIndexPath:index_path)
+    cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:CELL_ID)
+    cell.textLabel.text = @item[index_path.row]
+    cell
+  end
+
+  def tableView(tv, didSelectRowAtIndexPath:index_path)
+    p @item[index_path.row]
   end
 end
